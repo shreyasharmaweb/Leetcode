70dcfb7184edb030b474ca10322340f1984e9176
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-10-11 09:51:26 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int fun(int ind,int W,int val[],int wt[],int dp[][]){
+          if(ind==0){
+              return (W/wt[ind])*val[ind];
+          }
+          
+          if(dp[ind][W]!=-1){
+              return dp[ind][W];
+          }
+          
+          int nottake=fun(ind-1,W,val,wt,dp)+0;
+          int take=Integer.MIN_VALUE;
+          if(wt[ind]<=W){
+              take=fun(ind,W-wt[ind],val,wt,dp)+val[ind];
+          }
+          
+           return  dp[ind][W]=Math.max(nottake,take);
+      }
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int dp[][]=new int[N][W+1];
+        for(int i=0;i<N;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        
+        return fun(N-1,W,val,wt,dp);
+    }
+}
