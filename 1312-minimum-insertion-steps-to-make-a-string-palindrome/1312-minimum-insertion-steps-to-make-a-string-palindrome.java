880cf43384e4893d30d3bcf515ada16616beea34
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-10-16 10:32:00 +0530

    Time: 47 ms (5.23%), Space: 44.9 MB (39.85%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -7,7 +7,7 @@ class Solution {
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-        if (str1.charAt(ind1) == str2.charAt(ind2)) {
+        if (str1.charAt(ind1-1) == str2.charAt(ind2-1)) {
             return  dp[ind1][ind2]=1 + fun(ind1 - 1, ind2 - 1, str1, str2,dp);
         }
         else{
@@ -18,11 +18,26 @@ class Solution {
     public int minInsertions(String s) {
         StringBuilder str1 = new StringBuilder(s);
         StringBuilder str2 = new StringBuilder(s).reverse();
-        int dp[][]=new int[str1.length()][str2.length()];
-        for(int i=0;i<str1.length();i++){
-            Arrays.fill(dp[i],-1);
+        int dp[][]=new int[str1.length()+1][str2.length()+1];
+        for(int i=0;i<=str1.length();i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=str2.length();i++){
+            dp[0][i]=0;
         }
 
-        return s.length() - fun(s.length() - 1, s.length() - 1, str1, str2,dp);
+        for(int ind1=1;ind1<=str1.length();ind1++){
+            for(int ind2=1;ind2<=str2.length();ind2++){
+               
+               if (str1.charAt(ind1-1) == str2.charAt(ind2-1)) {
+                      dp[ind1][ind2]=1 + dp[ind1 - 1][ ind2 - 1];
+                        }
+               else{
+                   dp[ind1][ind2]=Math.max(dp[ind1 - 1][ ind2], dp[ind1][ind2 - 1]);
+                 }
+
+            }
+        }
+        return s.length()-dp[str1.length()][str2.length()];
     }
 }
