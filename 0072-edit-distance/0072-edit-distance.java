84cf9b8bc422daf787af4c18a5be631f284743e2
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-10-17 12:48:10 +0530

    Time: 3 ms (96.10%), Space: 43.6 MB (56.43%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    int fun(int ind1,int ind2,String str1,String str2,int dp[][]){
+        
+        if(ind2<0){
+          return ind1+1;
+        }
+        if(ind1<0){
+          return  ind2+1;
+        }
+        if(dp[ind1][ind2]!=-1){
+          return dp[ind1][ind2];
+        }
+        if(str1.charAt(ind1)==str2.charAt(ind2)){
+            return dp[ind1][ind2]= 0+fun(ind1-1,ind2-1,str1,str2,dp);
+        }
+        else{
+        return dp[ind1][ind2]= Math.min(Math.min(1+fun(ind1,ind2-1,str1,str2,dp),1+fun(ind1-1,ind2,str1,str2,dp)),1+fun(ind1-1,ind2-1,str1,str2,dp));
+        }
+    }
+
+    public int minDistance(String word1, String word2) {
+        int dp[][]=new int[word1.length()][word2.length()];
+        for(int i=0;i<dp.length;i++){
+           Arrays.fill(dp[i],-1);
+        }
+
+        return fun(word1.length()-1,word2.length()-1,word1,word2,dp);
+    }
+}
