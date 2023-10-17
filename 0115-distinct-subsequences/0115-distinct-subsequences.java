4a39eee2804c727b9f5601f409f95923fa202ac2
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-10-17 10:44:15 +0530

    Time: 17 ms (29.09%), Space: 46.6 MB (79.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -9,7 +9,7 @@ class Solution {
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-        if(s.charAt(ind1)==t.charAt(ind2)){
+        if(s.charAt(ind1-1)==t.charAt(ind2-1)){
            return  dp[ind1][ind2]= fun(ind1-1,ind2-1,s,t,dp)+fun(ind1-1,ind2,s,t,dp);
         }
         else{
@@ -17,12 +17,26 @@ class Solution {
         }
     }
     public int numDistinct(String s, String t) {
-        int dp[][]=new int [s.length()][t.length()];
+        int dp[][]=new int [s.length()+1][t.length()+1];
          
-         for(int i=0;i<dp.length;i++){
-             Arrays.fill(dp[i],-1);
+         for(int i=0;i<=s.length();i++){
+             dp[i][0]=1;
+         }
+         for(int j=1;j<=t.length();j++){
+             dp[0][j]=0;
          }
 
-        return fun(s.length()-1,t.length()-1,s,t,dp);
+         for(int ind1=1;ind1<=s.length();ind1++){
+             for(int ind2=1;ind2<=t.length();ind2++){
+
+                if(s.charAt(ind1-1)==t.charAt(ind2-1)){
+                        dp[ind1][ind2]= dp[ind1-1][ind2-1]+dp[ind1-1][ind2];
+                          }
+               else{
+                         dp[ind1][ind2]= dp[ind1-1][ind2];
+                   }
+             }
+         }
+         return dp[s.length()][t.length()];
     }
 }
