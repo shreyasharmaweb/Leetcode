7836917c2b68a970a887dba702e459a788866c82
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-10-24 15:00:19 +0530

    Time: 71 ms (29.71%), Space: 62.7 MB (31.98%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -19,12 +19,21 @@ class Solution {
         }
     }
     public int maxProfit(int[] prices) {
-        int dp[][][]=new int[prices.length][2][3];
-         for(int i=0;i<prices.length;i++){
-             for(int j=0;j<2;j++){
-                 Arrays.fill(dp[i][j],-1);
-             }
-         }
-        return fun(0,1,2,prices,dp);
+        int dp[][][]=new int[prices.length+1][2][3];
+        
+        for(int ind=prices.length-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                      
+                if(buy==1){
+                     dp[ind][buy][cap]=Math.max(-prices[ind]+dp[ind+1][0][cap],0+dp[ind+1][1][cap]);
+                    }
+                  else{
+                    dp[ind][buy][cap]=Math.max(prices[ind]+dp[ind+1][1][cap-1],0+dp[ind+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 }
