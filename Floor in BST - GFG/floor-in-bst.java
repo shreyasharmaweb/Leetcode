2e09c8b04312fe98a2cd99004dd9fe10c00893b4
shreyasharmaweb <136865533+shreyasharmaweb@users.noreply.github.com>
2023-12-30 12:40:55 +0530

    Added solution - LeetHub

--- Floor in BST - GFG/floor-in-bst.java
+++ Floor in BST - GFG/floor-in-bst.java
@@ -64,17 +64,18 @@ class Solution {
     public static int floor(Node root, int x) {
         // Code here
         int floor=-1;
+        
         while(root!=null){
             if(root.data==x){
                 floor=root.data;
                 return floor;
             }
-            if(root.data>x){
-                root=root.left;
-            }
-            else{
+            else if(root.data<x){
                 floor=root.data;
                 root=root.right;
+            }
+            else{
+                root=root.left;
             }
         }
         return floor;
